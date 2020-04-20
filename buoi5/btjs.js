94cd73991a04b2d440cff12e665831873c657b57
your username <dungnt33@topica.edu.vn>
2020-04-20 17:03:03 +0700

    test push

--- buoi5/btjs.js
+++ buoi5/btjs.js
@@ -1,12 +1,12 @@
-//cau1
-var Array = [5, 6, 78, 10];
-var cauA = Array[1] + Array[2];
-var cauB = cauA * 5;
-var cauC = Array[2] / 3;
-console.log(cauA, cauB, cauC);
+// //cau1
+// var Array = [5, 6, 78, 10];
+// var cauA = Array[1] + Array[2];
+// var cauB = cauA * 5;
+// var cauC = Array[2] / 3;
+// console.log(cauA, cauB, cauC);
 
 
-//cau 2
+// //cau 2
 var car = {
     hangXe: 'Audi',
     xuatXu: 'US',
@@ -14,4 +14,21 @@ var car = {
     soCho: 5,
     mauXe: ['đen','đỏ']
 }
-console.log(car);
+var car2 = {
+    hangXe: 'Audi',
+    xuatXu: 'US',
+    giaTien: '1000$',
+    soCho: 5,
+    mauXe: ['đen','đỏ']
+}
+// console.log(car);
+
+// var a = 1;
+// var b = '2';
+// var c = a + b;
+// console.log(c);
+// console.log(undefined==null);
+// var abc;
+// console.log(abc);
+console.log(1==='1');
+window
